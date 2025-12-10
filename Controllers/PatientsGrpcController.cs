3333bbf08cfe8e6d6ab1f59598962c860578bb1f
Andrada <ag.andrada.grosu@gmail.com>
2025-12-10 23:55:37 +0200

    cerinta 3

---
+++ Controllers/PatientsGrpcController.cs
@@ -0,0 +1,17 @@
+﻿using Grpc.Net.Client;
+using GrpcPatientService;
+using LibraryModel.Models;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Grosu_Andrada_ClinicAppointments.Controllers
+{
+    public class PatientsGrpcController : Controller
+    {
+        private readonly GrpcChannel channel;
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
